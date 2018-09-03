109efaa29bf67328c9980bb153159cdf555233ee
Arif Khan <arifkhan.incaendo@gmail.com>
2018-09-03 09:51:25 +0530

    First commit

---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Router } from '@angular/router';
+
+import { Employee } from '../models/employee.model';
+import { Department } from '../models/department.model';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  datePickerConfig: Partial<BsDatepickerConfig>;
+
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Paroll'},
+  ];
+
+  previewPhoto = false;
+
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    email: null,
+    phoneNumber: null,
+    contactPreference: null,
+    dateOfBirth: null,
+    department: -1,
+    isActive: null,
+    photoPath: null,
+  };
+
+  constructor(private _employeeService: EmployeeService, private _router: Router) {
+    this.datePickerConfig = Object.assign({}, {
+      containerClass: 'theme-dark-blue',
+      dateInputFormat: 'DD/MM/YYYY'
+    });
+
+  }
+
+  ngOnInit() {
+  }
+
+  saveEmployee(): void {
+    this._employeeService.save(this.employee);
+    this._router.navigate(['list']);
+  }
+
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
+}
---
+++ src/app/employees/employee.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Injectable()
+export class EmployeeService {
+  private listEmployees: Employee[] = [
+    {
+      id: 1,
+      name: 'Furqan',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'furqan@kt.com',
+      phoneNumber: 9876543210,
+      dateOfBirth: new Date('10/25/1988'),
+      department: 3,
+      isActive: true,
+    },
+    {
+      id: 2,
+      name: 'Arif',
+      gender: 'Male',
+      contactPreference: 'Email',
+      phoneNumber: 9876543210,
+      email: 'arif@kt.com',
+      dateOfBirth: new Date('04/01/1993'),
+      department: 2,
+      isActive: true,
+    }
+  ];
+
+  getEmployees(): Employee[] {
+    return this.listEmployees;
+  }
+
+  save(employee: Employee) {
+    console.log(employee);
+    this.listEmployees.push(employee);
+  }
+}
